b941009b15d64a33d7d0ae063995c7c6e9a2dcc9
sjwilczynski <sjwilczynski@gmail.com>
2018-01-23 22:22:59 +0100

    added subtract operation

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
 def process_request(params, operation):
     if operation == 'add':
         return params['a'] + params['b']
+    if operation == 'subtract':
+        return params['a'] - params['b']
--- test_helper.py
+++ test_helper.py
@@ -5,3 +5,9 @@ def test_helper_add():
     params = {'a': 10, 'b': 15}
     operation = 'add'
     assert process_request(params, operation) == 25
+
+
+def test_helper_subtract():
+    operation = 'subtract'
+    params = {'a': 20, 'b': 15}
+    assert process_request(params, operation) == 5
